b8dfbb41f973e9cfd168cbd7111217324062c4a2
Gabriel Cardoso <gabriel.cardoso@atos.net>
2023-12-01 17:52:08 -0300

    dev: add config-server,gateway,service-delivary,api-user

---
+++ service-discovery/src/test/java/com/chatgv/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatgv.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
